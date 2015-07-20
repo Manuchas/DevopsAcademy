2d895e8eba890dbfbda853d25a27596fd8668027
EC2 Default User <ec2-user@ip-172-31-18-121.us-west-2.compute.internal>
2015-07-20 05:25:05 +0000

    ido

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "this is a test file for push into git repository"
+#command to retrieve repo: git clone https://github.com/Manuchas/DevopsAcademy.git
+#use git push to upload the file
